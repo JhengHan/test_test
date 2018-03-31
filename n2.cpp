2f5ff175854ee647bef977e392f2563e060a6e50
JhengHan <ga830929@gmail.com>
2018-03-31 10:50:44 +0800

    factorial
    Test2

---
+++ n2.cpp
@@ -0,0 +1,38 @@
+// n2.cpp : 定義主控台應用程式的進入點。
+//
+
+// n1.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void fac(int, int *);
+
+
+int main(void)
+{
+	int n, result;
+
+	int i;
+	for (i = 1; i <= 10; i++)
+	{
+		result = 1;
+		fac(i, &result);
+	}
+	return 0;
+}
+
+void fac(int n, int *ans)
+{
+	int i;
+	for (i = 1; i <= n; i++)
+		*ans = *ans * i;
+	printf("%d\n", *ans);
+}
+
+
+
+
